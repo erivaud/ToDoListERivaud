e0af331c4914cd1790151f616ec0ce0aebe4e0d2
Estelle Rivaud <stl.rivaud@gmail.com>
2019-06-12 23:37:10 +0200

    ListAdapter en cours de création

--- app/src/main/java/com/example/todolisterivaud/MyListesAdapter.java
+++ app/src/main/java/com/example/todolisterivaud/MyListesAdapter.java
@@ -1,25 +1,36 @@
 package com.example.todolisterivaud;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-import com.example.todolisterivaud.Model.Listes;
+import com.example.todolisterivaud.Model.ToDoList;
 
-class MyListesAdapter extends ArrayAdapter<Listes> {
+import java.util.ArrayList;
+
+class MyListesAdapter extends ArrayAdapter<ToDoList> {
 
     public MyListesAdapter(Context c) {
         super(c, 0);
     }
 
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        LayoutInflater inflater = LayoutInflater.from(this.getContext());
-//        View v = inflater.inflate(R.layout.activity_liste_details, null);
+
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(this.getContext());
+        View view = inflater.inflate(R.layout.activity_liste_details, null);
+
+        ToDoList data = this.getItem(position);
+        TextView listeName = view.findViewById(R.id.listeName);
+//        ListView listeItems = view.findViewById(R.id.listeItems);
 //
-//        Listes data = this.getItem(position);
-//        ToDoList toDoLists = data.getTodoListes();
-//        TextView listeName = v.findViewById(R.id.listeName);
-//        ListView listeItems = v.findViewById(R.id.listeItems);
-//
-//        listeName.setText(toDoLists.getName());
-//    }
+        listeName.setText(data.getName());
+        return view;
+    }
+    //TODO : soit implémenter cette méthode, soit trouver une autre solution de parse
+    public void addAll(ArrayList<String> todoListsNames) {
+    }
 }
--- app/src/main/java/com/example/todolisterivaud/ShowLists.java
+++ app/src/main/java/com/example/todolisterivaud/ShowLists.java
@@ -3,6 +3,8 @@ package com.example.todolisterivaud;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,23 +16,40 @@ public class ShowLists extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_lists);
-        TextView userName = this.findViewById(R.id.showListUserName);
+
         Intent intent = getIntent();
+        String intentUserName = intent.getStringExtra("userName");
         ArrayList<String> todoListsNames = intent.getStringArrayListExtra("listesNamesArrayList");
+
+
+
         String todoListsNameConcat="";
         for (String todoListsName: todoListsNames) {
             todoListsNameConcat+=todoListsName+", ";
         }
         Toast.makeText(this, "" + todoListsNameConcat, Toast.LENGTH_LONG).show();
 
-        String intentUserName = intent.getStringExtra("userName");
+        TextView userName = this.findViewById(R.id.showListUserName);
+
 
         if (intentUserName != null){
             userName.setText(intentUserName);
         } else {
             userName.setText("empty UserName");
         }
-
+        if (todoListsNames !=null){
+            /*TextView listesVides = this.findViewById(R.id.listesVides);
+            listesVides.setVisibility(View.VISIBLE);
+            listesVides.setText(todoListsNameConcat);*/
+            ListView listLists = this.findViewById(R.id.listsList);
+            MyListesAdapter myListesAdapter = new MyListesAdapter(this.getApplicationContext());
+            myListesAdapter.addAll(todoListsNames);
+            listLists.setAdapter(myListesAdapter);
+        } else {
+            TextView listesVides = this.findViewById(R.id.listesVides);
+            listesVides.setVisibility(View.VISIBLE);
+            listesVides.setText("Listes Vides");
+        }
 
     }
 
