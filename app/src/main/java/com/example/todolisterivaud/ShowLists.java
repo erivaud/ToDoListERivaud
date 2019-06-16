ba1e93c779f128b7608620492e78502d48101aad
Estelle Rivaud <stl.rivaud@gmail.com>
2019-06-16 10:17:17 +0200

    affichage dans ArrayAdapter

--- app/src/main/java/com/example/todolisterivaud/ShowLists.java
+++ app/src/main/java/com/example/todolisterivaud/ShowLists.java
@@ -3,16 +3,32 @@ package com.example.todolisterivaud;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.todolisterivaud.Model.ToDoList;
 
+import java.util.ArrayList;
+
 public class ShowLists extends AppCompatActivity {
+
+    ListView listView;
+
+    public ArrayList<String> getTitles(ToDoList[] toDoLists){
+        ArrayList<String> titles = new ArrayList<>();
+        for (ToDoList liste : toDoLists) {
+            titles.add(liste.getName());
+        }
+        return titles;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_lists);
+
+        listView = (ListView) findViewById(R.id.listsList);
 
         Intent intent = getIntent();
         if (intent != null) {
@@ -21,30 +37,22 @@ public class ShowLists extends AppCompatActivity {
             ToDoList toDoList0 = intent.getParcelableExtra("toDoListe0");
             ToDoList toDoList1 = intent.getParcelableExtra("toDoListe1");
 
+            ToDoList[] toDoLists = new  ToDoList[]{toDoList0, toDoList1};
+
+
+            ArrayList<String> titles = new ArrayList<>(getTitles(toDoLists));
+
+
+
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles);
+            listView.setAdapter(arrayAdapter);
+
 
        /*Toast.makeText(this, "" + todoListsNameConcat, Toast.LENGTH_LONG).show(); */
 
             TextView userName = this.findViewById(R.id.showListUserName);
-            userName.setText(toDoList0.getName());
+            userName.setText(toDoList0.getName()+ "\n"+toDoList1.getName() + "\n" + toDoLists.length);
 
-          /*  if (intentUserName != null) {
-                userName.setText(intentUserName);
-            } else {
-                userName.setText("empty UserName");
-            } */
-       /* if (todoListsNames !=null){
-            TextView listesVides = this.findViewById(R.id.listesVides);
-            listesVides.setVisibility(View.VISIBLE);
-            listesVides.setText(todoListsNameConcat);
-            ListView listLists = this.findViewById(R.id.listsList);
-            MyListesAdapter myListesAdapter = new MyListesAdapter(this.getApplicationContext());
-            //myListesAdapter.addAll(todoListsNames);
-            //listLists.setAdapter(myListesAdapter);
-        } else {
-            TextView listesVides = this.findViewById(R.id.listesVides);
-            listesVides.setVisibility(View.VISIBLE);
-            listesVides.setText("Listes Vides");
-        }*/
         }
     }
 
